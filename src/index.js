eef833b7524a6fedaabf1f6affbd7c3c10a64551
Szymon  Ludwikowski <ludwikowskiszym1993@gmail.com>
2022-12-06 21:34:40 +0100

    `react-redux` configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './state'
+
 import App from './App'
 
 import './index.css'
@@ -8,12 +12,18 @@ import './index.css'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import { theme } from './theme'
 
+const store = configureStore({
+  reducer: { cart: cartReducer() }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App/>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App/>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
